dbc54a9aeec27c2316d8d40bbb1cb0cd9e699452
YongxiangJ <yongxiangj1999@gmail.com>
2024-09-19 17:31:46 +0200

    Forgot Password: Add a text when user enters wrong email or incomplete email. And fixed timer.

--- Project_DH/Authentication/View/ForgotPasswordView.swift
+++ Project_DH/Authentication/View/ForgotPasswordView.swift
@@ -14,62 +14,88 @@ import SwiftUI
 /// - Returns: none
 struct ForgotPasswordView: View {
     @StateObject var authViewModel = SignInViewModel()
-    @State private var refreshTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @ObservedObject private var timeManager = TimeManager.sharedTimer
+//    @State private var refreshTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @Environment(\.dismiss) private var dismiss
+    @State private var errorMessage = " "
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("password_reset_instructions")
-                    .font(.subheadline)
-                    .padding(.vertical, 60)
-                    .padding(.horizontal, 20)
-                
-                Spacer()
-                
-                HStack {
-                    Image(systemName: "envelope")
-                        .padding(.leading, 10)
-                    TextField("email", text: $authViewModel.email)
-                        .textInputAutocapitalization(.never)
-                        .keyboardType(.emailAddress)
+            GeometryReader { _ in
+                VStack {
+                    Text("password_reset_instructions")
                         .font(.subheadline)
-                        .padding(12)
-                }
-                .background(Color(.systemGray6))
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .padding(.horizontal, 30)
-                .padding(.bottom, 10)
-                
-                Spacer()
-                
-                
-                if !TimeManager.sharedTimer.isButtonEnabled {
-                    Text("remaining_seconds \(TimeManager.sharedTimer.timeRemaining)")
-                        .onReceive(refreshTimer) { _ in
-                            
+                        .padding(.vertical, 60)
+                        .padding(.horizontal, 20)
+                    
+                    HStack {
+                        Image(systemName: "envelope")
+                            .padding(.leading, 10)
+                        TextField("email", text: $authViewModel.email)
+                            .textInputAutocapitalization(.never)
+                            .keyboardType(.emailAddress)
+                            .font(.subheadline)
+                            .padding(12)
+                    }
+                    .background(Color(.systemGray6))
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .padding(.horizontal, 30)
+                    .padding(.bottom, 10)
+                    
+                    HStack { // ERROR MESSAGE
+                        errorMessage != " " ? Text(errorMessage) : Text(" ")
+                        Spacer()
+                    }
+                    .font(.footnote)
+                    .foregroundStyle(.brandRed)
+                    .padding(.leading, 30)
+                    
+                    Spacer()
+                    
+                    Image("resetPassword")
+                        .resizable()
+                        .frame(width: 260, height: 260)
+                        .clipShape(Circle())
+                        .opacity(0.5)
+                    
+                    
+                    Spacer()
+                    
+                    if !timeManager.isButtonEnabled {
+                        Text("remaining_seconds \(timeManager.timeRemaining)")
+                            .font(.headline)
+                            .foregroundStyle(.gray)
+                            .onReceive(timeManager.$timeRemaining) { _ in
+                                // This ensures the view listens for updates
+                            }
+                    }
+                    
+                    Button(action: {
+                        Task {
+                            if authViewModel.email.isValidEmailFormat {
+                                try await authViewModel.resetPassword()
+                                TimeManager.sharedTimer.isButtonEnabled = false
+                                TimeManager.sharedTimer.startTimer()
+                                errorMessage = " "
+                            } else {
+                                errorMessage = NSLocalizedString("Invalid email, please try again.", comment: "")
+                            }
                         }
+                    }) {
+                        Text("send_reset_link                                                     ")
+                    }
+                    .disabled(!TimeManager.sharedTimer.isButtonEnabled)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 310, height: 45)
+                    .background(Color(.brandDarkGreen).opacity(TimeManager.sharedTimer.isButtonEnabled ? 1 : 0.5))
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .padding(.vertical)
+                    
+                    Spacer()
                 }
-                
-                Button(action: {
-                    Task {try await authViewModel.resetPassword() }
-                    TimeManager.sharedTimer.isButtonEnabled = false
-                    TimeManager.sharedTimer.startTimer()
-                }) {
-                    Text("send_reset_link                                                     ")
-                }
-                .disabled(!TimeManager.sharedTimer.isButtonEnabled)
-                .fontWeight(.semibold)
-                .foregroundStyle(.white)
-                .frame(width: 310, height: 45)
-                .background(.brandDarkGreen)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .padding(.vertical)
-                
-                Spacer()
             }
             .ignoresSafeArea(.keyboard)
-            
         }// End of Navigation Stack
         .navigationTitle("reset_password")
         .navigationBarTitleDisplayMode(.large)
